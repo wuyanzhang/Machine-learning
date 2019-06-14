81c71ea69154dd88e932600e69babe0924522ffd
wyz <178172535@qq.com>
2019-06-14 18:10:10 +0800

    AdaBoost

---
+++ AdaBoost/adboost.py
@@ -0,0 +1,116 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 函数说明：创建单层决策树的数据集
+def loadSimpleData():
+    datMat = np.mat([[1., 2.1],
+                        [1.5, 1.6],
+                        [1.3, 1.],
+                        [1., 1.],
+                        [2., 1.]])
+    classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
+    return datMat, classLabels
+
+# 函数说明：数据可视化
+def showDataSet(dataMat,labelMat):
+    data_plus = []  # 正样本
+    data_minus = []  # 负样本
+    for i in range(len(dataMat)):
+        if labelMat[i] > 0:
+            data_plus.append(dataMat[i])
+        else:
+            data_minus.append(dataMat[i])
+    data_plus_np = np.array(data_plus)  # 转换为numpy矩阵
+    data_minus_np = np.array(data_minus)  # 转换为numpy矩阵
+    plt.scatter(np.transpose(data_plus_np)[0], np.transpose(data_plus_np)[1])  # 正样本散点图
+    plt.scatter(np.transpose(data_minus_np)[0], np.transpose(data_minus_np)[1])  # 负样本散点图
+    plt.show()
+
+# 函数说明：单层决策树分类函数
+def stumpClassify(dataMatrix,dimen,threshVal,threshIneq):
+    retArray = np.ones((np.shape(dataMatrix)[0],1))
+    if threshIneq == 'lt':
+        retArray[dataMatrix[:,dimen] <= threshVal] = -1.0
+    else:
+        retArray[dataMatrix[:, dimen] > threshVal] = -1.0
+    return retArray
+
+# 函数说明：找到数据集上最佳的单层决策树
+def buildStump(dataArr,classLabels,D):
+    dataMatrix = np.mat(dataArr)
+    labelMat = np.mat(classLabels).T
+    m,n = np.shape(dataMatrix)
+    numSteps = 10.0
+    bestStump = {}
+    minError = float('inf')
+    bestClasEst = np.mat(np.zeros((m,1)))
+    for i in range(n):
+        rangeMin = dataMatrix[:,i].min()
+        rangeMax = dataMatrix[:,i].max()
+        stepSize = (rangeMax - rangeMin) / numSteps
+        for j in range(-1,int(numSteps)+1):
+            for inequal in ['lt','gt']:
+                threshVal = (rangeMin + float(j)*stepSize)
+                predictedVals = stumpClassify(dataMatrix,i,threshVal,inequal)
+                errArr = np.mat(np.ones((m,1)))
+                errArr[predictedVals == labelMat] = 0
+                weightedError = D.T * errArr
+                print("split: dim %d, thresh %.2f, thresh ineqal: %s, the weighted error is %.3f" % (
+                i, threshVal, inequal, weightedError))
+                if weightedError < minError:
+                    minError = weightedError
+                    bestClasEst = predictedVals.copy()
+                    bestStump['dim'] = i
+                    bestStump['thresh'] = threshVal
+                    bestStump['ineq'] = inequal
+    return bestStump, minError, bestClasEst
+
+# 函数说明：使用AdaBoost算法提升弱分类器性能
+def adaBoost(dataArr,classLabels,numIt = 40):
+    weakClassArr = []
+    m = np.shape(dataArr)[0]
+    D = np.mat(np.ones((m, 1)) / m)
+    aggClassEst = np.mat(np.zeros((m,1)))
+    for i in range(numIt):
+        bestStump, error, classEst = buildStump(dataArr, classLabels, D)
+        print("D:", D.T)
+        alpha = float(0.5 * np.log((1.0 - error) / max(error, 1e-16)))
+        bestStump['alpha'] = alpha
+        weakClassArr.append(bestStump)
+        print("classEst: ", classEst.T)
+        expon = np.multiply(-1 * alpha * np.mat(classLabels).T, classEst)  # 计算e的指数项
+        D = np.multiply(D, np.exp(expon))
+        D = D / D.sum()  # 根据样本权重公式，更新样本权重
+        # 计算AdaBoost误差，当误差为0的时候，退出循环
+        aggClassEst += alpha * classEst
+        print("aggClassEst: ", aggClassEst.T)
+        aggErrors = np.multiply(np.sign(aggClassEst) != np.mat(classLabels).T, np.ones((m, 1)))  # 计算误差
+        errorRate = aggErrors.sum() / m
+        print("total error: ", errorRate)
+        if errorRate == 0.0: break  # 误差为0，退出循环
+    return weakClassArr, aggClassEst
+
+# 函数说明：测试
+# datToClass - 待分类样例
+# classifierArr - 训练好的分类器
+def adaClassify(datToClass,classifierArr):
+    dataMatrix = np.mat(datToClass)
+    m = np.shape(dataMatrix)[0]
+    aggClassEst = np.mat(np.zeros((m, 1)))
+    for i in range(len(classifierArr)):
+        classEst = stumpClassify(dataMatrix, classifierArr[i]['dim'], classifierArr[i]['thresh'], classifierArr[i]['ineq'])
+        aggClassEst += classifierArr[i]['alpha'] * classEst
+    return np.sign(aggClassEst)
+
+if __name__ == '__main__':
+    dataArr,classLabels = loadSimpleData()
+    weakClassArr,aggClassEst = adaBoost(dataArr,classLabels)
+    print(adaClassify([[0, 0], [5, 5]], weakClassArr))
+    # print(weakClassArr) # 每一个弱分类器的权重信息
+    # print(aggClassEst) # 最后的预测分类标签
+    # D = np.mat(np.ones((5,1))/5)
+    # bestStump,minError,bestClasEst = buildStump(dataArr,classLabels,D)
+    # print('bestStump:\n', bestStump)
+    # print('minError:\n', minError)
+    # print('bestClasEst:\n', bestClasEst)
+    # showDataSet(dataArr,classLabels)
